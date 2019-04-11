c2c6ae8ef19ed3417b0ce4aac72e5969f1ca44af
DylanWard14 <dylan.ward@iinet.net.au>
2019-04-11 10:49:36 +1000

    set up functionality to send your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,3 +9,14 @@ document.querySelector('#messageForm').addEventListener('submit', (e) => {
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message)
 })
+
+document.querySelector('#sendLocation').addEventListener('click', () => {
+    if (!navigator.geolocation)
+    {
+        return alert('Getlocation is no supported by your browser!');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {latitude: position.coords.latitude, longitude: position.coords.longitude})
+    })
+})
--- src/index.js
+++ src/index.js
@@ -18,9 +18,18 @@ io.on('connection', (socket) => {
     console.log('new websocket connection')
 
     socket.emit('message', welcomeMessage);
+    socket.broadcast.emit('message', "A new user has joined!")
 
     socket.on('sendMessage', (message) => {
         io.emit('message', message)
+    })
+
+    socket.on('sendLocation', (coordinates) => {
+        io.emit('message', `https://google.com/maps?q=${coordinates.latitude},${coordinates.longitude}`);
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', "A user has left!");
     })
 })
 
