1c2225e7d0287af8435b2227a80e25eca9b85d8c
DylanWard14 <dylan.ward@iinet.net.au>
2019-04-11 14:40:38 +1000

    Implemented autoscroll feature

--- src/index.js
+++ src/index.js
@@ -28,25 +28,31 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
-        socket.emit('message', generateMessage('Welcome!'));
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`));
+        socket.emit('message', generateMessage("Admin", 'Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage("Admin", `${user.username} has joined!`));
+        io.to(user.room).emit('roomData', {
+            room: user.room,
+            users: getUsersInRoom(user.room)
+        });
 
         callback();
     })
 
     socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
         const filter = new Filter();
 
         if (filter.isProfane(message))
         {
             return callback('Profanity is not allowed!')
         }
-        io.to('Brisbane').emit('message', generateMessage(message))
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback('Delivered!');
     })
 
     socket.on('sendLocation', (coordinates, callback) => {
-        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coordinates.latitude},${coordinates.longitude}`));
+        const user = getUser(socket.id);
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coordinates.latitude},${coordinates.longitude}`));
         callback('Location Shared')
     })
 
@@ -55,7 +61,11 @@ io.on('connection', (socket) => {
 
         if(user)
         {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`));
+            io.to(user.room).emit('message', generateMessage("Admin", `${user.username} has left!`));
+            io.to(user.room).emit('roomData', {
+                room: user.room,
+                users: getUsersInRoom(user.room)
+            })
         }
     })
 })
