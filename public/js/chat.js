23d5110ecbf83a0a726fd4f1fbade9a8f9b96ea0
DylanWard14 <dylan.ward@iinet.net.au>
2019-04-11 11:14:36 +1000

    Made changes to forms and button states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,22 +1,48 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector('#messageForm');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.querySelector('#sendLocation');
+
 socket.on('message', (message) => {
     console.log(message);
 })
 
-document.querySelector('#messageForm').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+    //disable form
+    $messageFormButton.setAttribute('disabled', 'disabled');
     const message = e.target.elements.message.value;
-    socket.emit('sendMessage', message)
+
+    socket.emit('sendMessage', message, (error) => {
+        // enable form
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+        if(error)
+        {
+            return console.log(error);
+        }
+        console.log('message delivered');
+    })
 })
 
-document.querySelector('#sendLocation').addEventListener('click', () => {
+$sendLocationButton.addEventListener('click', () => {
     if (!navigator.geolocation)
     {
         return alert('Getlocation is no supported by your browser!');
     }
-
+    $sendLocationButton.setAttribute('disabled', 'disabled');
     navigator.geolocation.getCurrentPosition((position) => {
-        socket.emit('sendLocation', {latitude: position.coords.latitude, longitude: position.coords.longitude})
+        socket.emit('sendLocation', {latitude: position.coords.latitude, longitude: position.coords.longitude}, (error) => {
+            $sendLocationButton.removeAttribute('disabled');
+            if (error)
+            {
+                return console.log(error);
+            }
+            console.log('location shared');
+        })
     })
 })
